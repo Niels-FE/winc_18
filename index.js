8e54fd802ba36656582a348b04d3d2d070725c79
Niels-FE <89984377+Niels-FE@users.noreply.github.com>
2021-10-07 14:23:08 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+const age = 28;
+const isFemale = false;
+const driverStatus = 'bob';
+if (age >= 18){
+    console.log('je mag naar binnen');
+} else {
+    console.log('je bent te jong. Blijf buiten!');
+};
+if (isFemale) {
+    console.log('je bent een vrouw');
+} else {
+    console.log('je bent geen vrouw');
+};
+if (driverStatus == 'bob'){
+    console.log('je mag nog rijden ');
+} else {
+    console.log('je mag niet rijden');
+};
